f2137f05dbbc1ee84193d6b214f89868ed25c791
Monarch <monarchofaether@gmail.com>
2024-05-16 18:21:06 +0300

    init modified to initialize dict to attritubes

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,24 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """
     Defines all common attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    a = '%Y-%m-%dT%H:%M:%S.%f'
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime(value, self.a))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -35,3 +45,4 @@ class BaseModel:
         dict_rep["updated_at"] = self.updated_at.isoformat()
 
         return (dict_rep)
+
