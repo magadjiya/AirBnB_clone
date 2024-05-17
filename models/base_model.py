b2afb354809a3ee166b71bb337f52054bf86843a
Monarch <monarchofaether@gmail.com>
2024-05-17 17:20:49 +0300

    serialization/deserialization and persistent storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -9,6 +10,7 @@ class BaseModel:
     Defines all common attributes/methods for other classes
     """
     a = '%Y-%m-%dT%H:%M:%S.%f'
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -21,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -33,6 +36,7 @@ class BaseModel:
         """
         Updates attribute with current datetime
         """
+        storage.save()
         self.updated_at = datetime.now()
 
     def to_dict(self):
@@ -45,4 +49,3 @@ class BaseModel:
         dict_rep["updated_at"] = self.updated_at.isoformat()
 
         return (dict_rep)
-
