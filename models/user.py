6be973dd375042d92d0c09f1637070dc5e1e2d1f
Monarch <monarchofaether@gmail.com>
2024-05-19 12:59:45 +0300

    user created

--- models/user.py
+++ models/user.py
@@ -4,7 +4,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.email = ''
         self.password = ''
         self.first_name = ''
