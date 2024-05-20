c34310a7491fb6cdf3047f0b796e7c1e80f73458
Monarch <monarchofaether@gmail.com>
2024-05-20 04:28:38 +0300

    test classes created

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Test file for User Class"""
+import unittest
+
+
+class TestState(unittest.TestCase):
+    """
+    Tests the User data model
+    """
+
+    def setUp(self):
+        """
+        Test for all classes
+        """
+        pass
+
+    def test_attributes(self):
+        """
+        Tests attributes assignment with expected values
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
